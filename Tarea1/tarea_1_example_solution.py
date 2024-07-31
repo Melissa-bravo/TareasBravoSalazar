bedd4e21e90fd3a96a98d6d299f246350085f4cc
Mr.Jules <88597983+JulesSG@users.noreply.github.com>
2024-07-30 22:38:33 -0600

    Se cambio type por isinstance en las comprobaciones

--- Tarea1/tarea_1_example_solution.py
+++ Tarea1/tarea_1_example_solution.py
@@ -1,9 +1,11 @@
 def operation_selector(num1, num2, op):
     # Verifica si num1 y num2 son enteros
-    if type(num1) != int or type(num2) != int:
+    if not isinstance(num1, int) or not isinstance(num2, int):
         return -50, None  # Código de error -50 si alguno no es entero
+    if isinstance(num1, bool) or isinstance(num2, bool):
+        return -50, None
     # Verifica si op es una string
-    if type(op) != str:
+    if not isinstance(op, str):
         return -60, None  # Código de error -60 si op no es una string
     # Verifica si op es una operación válida
     if op not in ["+", "-", "*", "&"]:
@@ -23,11 +25,10 @@ def operation_selector(num1, num2, op):
 def calculo_promedio(lista):
     # Verifica si todos los elementos en la lista son enteros
     for i in lista:
-        if type(i) != int:
+        if not isinstance(i, int) or isinstance(i, bool):
             return -80, None  # Error si algún elemento no es entero
     # Verifica si la longitud de la lista es mayor a 10
     if len(lista) > 10:
         return -90, None  # Error de la lista si tiene mas de 10 elementos
     # Calcula y devuelve el promedio de la lista
     return 0, sum(lista) / len(lista)
-
